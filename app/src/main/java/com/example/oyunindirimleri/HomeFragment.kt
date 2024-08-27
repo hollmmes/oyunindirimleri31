23477a6a9882e7c75de4b9cf0d2c10e5fd9d812c
Tufancan "Hollmmes" Demirkılıç <tufancanedu@gmail.com>
2024-08-27 15:12:48 +0300

    update buttons

--- app/src/main/java/com/example/oyunindirimleri/HomeFragment.kt
+++ app/src/main/java/com/example/oyunindirimleri/HomeFragment.kt
@@ -75,9 +75,6 @@ class HomeFragment : Fragment() {
 
 
 
-
-
-
         return view
     }
 
--- app/src/main/java/com/example/oyunindirimleri/MainActivity.kt
+++ app/src/main/java/com/example/oyunindirimleri/MainActivity.kt
@@ -1,15 +1,48 @@
 package com.example.oyunindirimleri
 
 import android.os.Bundle
-import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.view.ViewCompat
-import androidx.core.view.WindowInsetsCompat
+import androidx.navigation.NavController
+import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.ui.setupWithNavController
+import com.google.android.material.bottomnavigation.BottomNavigationView
+import com.google.android.material.floatingactionbutton.FloatingActionButton
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var navController: NavController
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
         setContentView(R.layout.activity_main)
+
+        // Find the NavHostFragment
+        val navHostFragment = supportFragmentManager
+            .findFragmentById(R.id.fragmentContainerView2) as NavHostFragment
+        navController = navHostFragment.navController
+
+        // Set up BottomNavigationView with NavController
+        val bottomNavigationView = findViewById<BottomNavigationView>(R.id.bottom_navigation)
+        bottomNavigationView.setupWithNavController(navController)
+
+        // Handle BottomNavigationView item selection
+        bottomNavigationView.setOnNavigationItemSelectedListener { item ->
+            when (item.itemId) {
+                R.id.home -> {
+                    navController.popBackStack(R.id.homeFragment, false)
+                    true
+                }
+                R.id.profile -> {
+                    navController.navigate(R.id.profileFragment)
+                    true
+                }
+                else -> false
+            }
+        }
+
+        // Handle Floating Action Button click
+        val fab = findViewById<FloatingActionButton>(R.id.fab)
+        fab.setOnClickListener {
+            navController.navigate(R.id.freeFragment)
         }
     }
+}
