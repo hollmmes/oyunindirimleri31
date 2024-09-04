a7c0e5cbfe58f214becbbec7a021e2e38ed25705
Tufancan "Hollmmes" Demirkılıç <tufancanedu@gmail.com>
2024-09-05 01:57:42 +0300

    fix issues

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -12,8 +12,8 @@ android {
         applicationId = "com.tufancan.oyunindirimleri"
         minSdk = 27
         targetSdk = 34
-        versionCode = 3
-        versionName = "1.3"
+        versionCode = 4
+        versionName = "1.4"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
